b0c3b3f56b3a2238b87f3d9912a8bf4cc59d6c23
Caleb Sayre <DrSayre2002@yahoo.com>
2016-06-09 00:05:23 -0400

    added some tests to user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test 'correct number of unassigned votes' do
+    assert_equal 20, users(:caleb).votes_not_assigned, 'Caleb has 10 votes not assigned'
+  end
+
+  test 'caleb thinks Trump will win Kentucky' do
+    assert users(:caleb).selected_candidate(states(:kentucky), candidates(:donald))
+  end
+
+  test 'caleb does not think Hillary will win Kentucky' do
+    assert_not users(:caleb).selected_candidate(states(:kentucky), candidates(:hillary))
+  end
+
+  test 'caleb thinks Trump will win 10 votes' do
+    assert_equal 10, users(:caleb).votes_for_candidate(candidates(:donald))
+  end
+
+  test 'caleb thinks Hillary will win 0 votes' do
+    assert_equal 0, users(:caleb).votes_for_candidate(candidates(:hillary))
+  end
 end
